31a76b612d3165d70f7ca7ce080e7898415ab998
Syafiq <syafiqmusoffa@gmail.com>
2025-09-30 14:52:24 +0700

    retry to deploy

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -2,14 +2,15 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import bcrypt from "bcrypt";
 
-export async function PATCH(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
+export async function PATCH(req: Request) {
   try {
+    const url = new URL(req.url);
+    const segments = url.pathname.split("/");
+    const id = segments[segments.length - 1]; // ambil id dari path
+
     const body = await req.json();
 
-    const dataToUpdate = { ...body };
+    const dataToUpdate: any = { ...body }; // bisa kasih type yang lebih spesifik kalau mau
 
     if (body.password) {
       const hashedPassword = await bcrypt.hash(body.password, 10);
@@ -17,13 +18,13 @@ export async function PATCH(
     }
 
     const user = await prisma.user.update({
-      where: { id: params.id },
+      where: { id },
       data: dataToUpdate,
     });
 
-
     return NextResponse.json(user);
-  } catch {
+  } catch (error: unknown) {
+    console.error("Update user error:", error);
     return NextResponse.json(
       { error: "Failed to update user" },
       { status: 500 }
