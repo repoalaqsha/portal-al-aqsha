ff654c527faa2b4adee49ca0dad06d676bdf4255
Syafiq <syafiqmusoffa@gmail.com>
2025-09-30 15:03:58 +0700

    retry to deploy

--- app/api/visi-misi/[id]/route.ts
+++ app/api/visi-misi/[id]/route.ts
@@ -1,18 +1,22 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-export async function PATCH(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
+export async function PATCH(req: Request) {
   try {
+    // ambil id dari path
+    const url = new URL(req.url);
+    const segments = url.pathname.split("/");
+    const id = segments[segments.length - 1];
+
     const body = await req.json();
+
     const visiMisi = await prisma.visiMisi.update({
-      where: { id: params.id },
+      where: { id },
       data: body,
     });
+
     return NextResponse.json(visiMisi);
-  } catch {
+  } catch  {
     return NextResponse.json(
       { error: "Failed to update visi & misi" },
       { status: 500 }
